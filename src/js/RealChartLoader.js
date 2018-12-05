6ea03248b659e36beef7b8478d5b5eb0eb37c5f7
Cameronpak <cameronandrewpak@gmail.com>
2018-12-05 14:59:30 -0600

    Testing out scaling on chartJS

--- src/js/RealChartLoader.js
+++ src/js/RealChartLoader.js
@@ -2,17 +2,38 @@ import ChartLoader from "./ChartLoader.js";
 
 export default class RealChartLoader extends ChartLoader {
   loadChart() {
-    setTimeout(this.simulateLoad, 500);
+    setTimeout(this.simulateLoad, 1000);
     console.log("This is a real chart");
   }
 
   loadRealChart(data) {
+    // convert data to format fit for chart
     let newData = data.map((object, index) => ({
        x: moment.unix(object.startTime).format('h:mm:ss a'),
        y: (object.isMoving ? 1 : 0)
      }));
 
-     console.log(newData);
+    console.log(newData);
+
+    var minutes = {
+      parser: "HH:mm:ss a", //<- use 'parser'
+      unit: 'minute',
+      unitStepSize: 1,
+      minUnit: 'minute',
+      displayFormats: {
+         'minute': 'hh:mm a',
+      }
+    };
+
+    var hours = {
+      parser: "HH a", //<- use 'parser'
+      unit: 'hour',
+      unitStepSize: 1,
+      minUnit: 'hour',
+      displayFormats: {
+         'hour': 'hh a',
+      }
+    };
 
     // display loading empty thing `#proxyChart`
     var proxyChart = document.getElementById('proxyChart');
@@ -42,14 +63,7 @@ export default class RealChartLoader extends ChartLoader {
               {
                 type: 'time',
                 // position: 'bottom',
-                time: {
-                  parser: "HH:mm:ss a", //<- use 'parser'
-                  unit: 'minutes',
-                  unitStepSize: 1,
-                  displayFormats: {
-                     'minutes': 'hh:mm a',
-                  }
-                },
+                time: minutes,
               }
             ],
           yAxes: [{
@@ -72,30 +86,5 @@ export default class RealChartLoader extends ChartLoader {
     proxyChart.classList.add("d-none"); /* makes it visible */
     var ctx = document.getElementById("realChart");
     ctx.classList.remove('d-none');
-    // var myChart = new Chart(ctx, {
-    //   type: 'line',
-    //   data: {
-    //     labels: ["1:00 AM", "1:01 AM", "1:05 AM", "1:10 AM", "1:11 AM", "1:12 AM"],
-    //     datasets: [{
-    //       label: '# of Votes',
-    //       steppedLine: 'true',
-    //       data: [1, 0, 1, 1, 0, 1],
-    //       backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    //       borderColor: 'rgba(255,99,132,1)',
-    //       borderWidth: 1
-    //     }]
-    //   },
-    //   options: {
-    //     scales: {
-    //       yAxes: [{
-    //         ticks: {
-    //           max: 1,
-    //           min: 0,
-    //           stepSize: 1
-    //         }
-    //       }]
-    //     }
-    //   }
-    // });
   }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -50,58 +50,15 @@ let activityTable = null;
 //let activityTable = coordAnalyzer.analyzeData(coordinateSets);
 //console.log(activityTable);
 
+// load chart initially
 chartLoader.loadChart();
 
+// receive file upload
 document.getElementById('input').addEventListener('change', () => {
   let file = event.target.files;
   console.log(file);
   handleFiles(file);
 });
-
-function populateGraph(myData) {
-  // var ctx = document.getElementById("myChart");
-  // var myChart = new Chart(ctx, {
-  //   type: 'bar',
-  //   data: {
-  //     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-  //     datasets: [{
-  //       label: '# of Votes',
-  //       data: [12, 19, 3, 5, 2, 3],
-  //       backgroundColor: [
-  //         'rgba(255, 99, 132, 0.2)',
-  //         'rgba(54, 162, 235, 0.2)',
-  //         'rgba(255, 206, 86, 0.2)',
-  //         'rgba(75, 192, 192, 0.2)',
-  //         'rgba(153, 102, 255, 0.2)',
-  //         'rgba(255, 159, 64, 0.2)'
-  //       ],
-  //       borderColor: [
-  //         'rgba(255,99,132,1)',
-  //         'rgba(54, 162, 235, 1)',
-  //         'rgba(255, 206, 86, 1)',
-  //         'rgba(75, 192, 192, 1)',
-  //         'rgba(153, 102, 255, 1)',
-  //         'rgba(255, 159, 64, 1)'
-  //       ],
-  //       borderWidth: 1
-  //     }]
-  //   },
-  //   options: {
-  //     scales: {
-  //       yAxes: [{
-  //         ticks: {
-  //           beginAtZero: true
-  //         }
-  //       }]
-  //     }
-  //   }
-  // });
-  // var x = document.getElementById('moveData');
-  // var myArray = myData._activities;
-  // myArray.forEach(object => {
-  // 	x.innerHTML += `<li>Is Moving: ${object.isMoving}\tStart Time: ${object.startTime}\tEnd Time: ${object.endTime}</li>`;
-  // });
-}
 
 function handleFiles(theFiles) {
   let file = theFiles[0];
@@ -110,37 +67,26 @@ function handleFiles(theFiles) {
     header: true,
     dynamicTyping: true,
     complete: function(results) {
-      //console.log('parsing complete read', results, 'records.');
+
+      // extract coordinates
       results.data.forEach(line => {
         let keys = Object.keys(line);
-        // TODO: Right here, convert the last CoordinateSet value to remove last three digits to convert
-        //  all coordinate sets to seconds. Then remove duplicates.
         var timeStamp = line[keys[3]];
-        // remove milliseconds and convert to seconds
-        timeStamp = (timeStamp - (timeStamp % 1000)) / 1000;
+        timeStamp = (timeStamp - (timeStamp % 1000)) / 1000; // milliseconds to seconds
         coordinateSets.push(new CoordinateSet(line[keys[0]], line[keys[1]], line[keys[2]], timeStamp));
       });
-
-      //console.log(coordinateSets);
       activityTable = coordAnalyzer.analyzeData(coordinateSets);
 
-      // console.log(activityTable);
-
-      /* remove duplicate times */
-      var table = activityTable._activities;
+      // remove duplicate times
+      var data = activityTable._activities;
       // I chose this method based on https://stackoverflow.com/a/18165553/6598861
-      for (let i = table.length; i--;) {
-        if (table[i].startTime === table[i].endTime) {
-            table.splice(i, 1);
+      for (let i = data.length; i--;) {
+        if (data[i].startTime === data[i].endTime) {
+          data.splice(i, 1);
         }
       }
 
-      chartLoader.loadRealChart(table);
-
-      console.log(activityTable);
-
-      // todo: test populating the graph
-      populateGraph(activityTable);
+      chartLoader.loadRealChart(data);
     }
   });
 }

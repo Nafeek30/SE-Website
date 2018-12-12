820ba372b6a36ea9895ef8db8f8f2aad64d61ebc
Canaan <canaanm1114@gmail.com>
2018-12-12 17:56:27 -0600

    Added test suite for Jerk

--- src/js/test/main.js
+++ src/js/test/main.js
@@ -1,7 +1,10 @@
 import Activity from "../Activity.js";
 import ActivityTable from "../ActivityTable.js";
+import JerkAnalyzerImplementor from "../JerkAnalyzerImplementor.js";
+import CoordinateSet from "../CoordinateSet.js";
 
-//example test
+
+/* //example test
 class Calculator{
 	add(a,b){
 		return a + b;
@@ -26,8 +29,7 @@ describe("calculatorSuite", () => {
 	it("should subtract two numbers from eachother", () => {
 		expect(calculator.subtract(2,2)).toBe(0);
 	});
-});
-
+}); */
 
 //Real Tests
 describe("ActivityTableSuite", () => {
@@ -48,6 +50,19 @@ describe("ActivityTableSuite", () => {
 	
 	it("should sum its activities' active times into a total active duration", ()=>{
 		expect(activityTable.getActiveDuration()).toBe(1);
+	});
+});
+
+describe("JerkAnalyzerImplementorSuite", () =>{
+	const coordinateSet1 = new CoordinateSet(2.3, 3.2, 2.33, 100);
+	const coordinateSet2 = new CoordinateSet(4.3, 4.2, 4.33, 200);
+	const coordinateSets = [];
+	coordinateSets.push(coordinateSet1);
+	coordinateSets.push(coordinateSet2);
+	const jerkAnalyzerImplementor = new JerkAnalyzerImplementor();
+	
+	it("should output an ActivityTable", () =>{
+		expect(jerkAnalyzerImplementor.analyzeData(coordinateSets)).toEqual(jasmine.any(ActivityTable));
 	});
 });
 		

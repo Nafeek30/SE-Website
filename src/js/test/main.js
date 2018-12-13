9123b3ad5ac09117278258319cc439ab2b5cefe4
Canaan <canaanm1114@gmail.com>
2018-12-12 19:23:14 -0600

    CoordinateSetAnalyzerSuite tests added

--- src/js/test/main.js
+++ src/js/test/main.js
@@ -2,34 +2,7 @@ import Activity from "../Activity.js";
 import ActivityTable from "../ActivityTable.js";
 import JerkAnalyzerImplementor from "../JerkAnalyzerImplementor.js";
 import CoordinateSet from "../CoordinateSet.js";
-
-
-/* //example test
-class Calculator{
-	add(a,b){
-		return a + b;
-	}
-	subtract(a,b){
-		return a - b;
-	}
-}
-		
-//describe(description, specDefinitions)
-//specDefinitions is simply a function containing tests for your class
-describe("calculatorSuite", () => {
-	const calculator = new Calculator();
-	
-	//it(description, testFunction *optional*, timeout *optional*)
-	it("should add two numbers together", () => {
-		//expect(thing).toBe(realThing);
-		//other versions of toBe:   toBeGreaterThan, toBeLessThan, https://jasmine.github.io/api/3.3/matchers.html
-		expect(calculator.add(2,2)).toBe(4);
-	});
-	
-	it("should subtract two numbers from eachother", () => {
-		expect(calculator.subtract(2,2)).toBe(0);
-	});
-}); */
+import CoordinateSetAnalyzer from "../CoordinateSetAnalyzer.js";
 
 //Real Tests
 describe("ActivityTableSuite", () => {
@@ -66,3 +39,20 @@ describe("JerkAnalyzerImplementorSuite", () =>{
 	});
 });
 		
+describe("CoordinateSetAnalyzerSuite", () => {
+	const coordinateSet1 = new CoordinateSet(2.3, 3.2, 2.33, 100);
+	const coordinateSet2 = new CoordinateSet(4.3, 4.2, 4.33, 200);
+	const coordinateSets = [];
+	coordinateSets.push(coordinateSet1);
+	coordinateSets.push(coordinateSet2);
+	const jerkAnalyzerImplementor = new JerkAnalyzerImplementor();
+	const coordinateSetAnalyzer = new CoordinateSetAnalyzer(jerkAnalyzerImplementor);
+	
+	it("should output an ActivityTable", () =>{
+		expect(coordinateSetAnalyzer.analyzeData(coordinateSets)).toEqual(jasmine.any(ActivityTable));
+	});
+	
+	it("should not exist without an implementor", () => {
+		expect(() => { return new CoordinateSetAnalyzer()}).toThrowError();
+	});
+});
